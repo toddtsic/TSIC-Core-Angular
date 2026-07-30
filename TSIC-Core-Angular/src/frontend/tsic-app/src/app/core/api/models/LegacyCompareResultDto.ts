660fce57b303ad3e5fbe53d704fed59d9ee88128
Todd <toddtsic@gmail.com>
2026-07-30 08:35:50 -0700

    fix(revenue): add missing LegacyCompareResultDto generated model

---
+++ TSIC-Core-Angular/src/frontend/tsic-app/src/app/core/api/models/LegacyCompareResultDto.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type LegacyCompareResultDto = {
+    pass: boolean;
+    rollupCellsLegacy: number;
+    rollupCellsNew: number;
+    materialMismatches: Array<string>;
+    pennyDeltas: Array<string>;
+};
+
